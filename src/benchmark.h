a2e34d849fd0cc941b1ca74e356ab18431d6c739
finwo <finwo@pm.me>
2023-03-25 00:24:18 +0100

    Made integrated MIN/MAX macros safe

--- src/benchmark.h
+++ src/benchmark.h
@@ -2,11 +2,11 @@
 #define __FINWO_BENCHMARK_H__
 
 #ifndef MIN
-#define MIN(a,b) ((a<b)?(a):(b))
+#define MIN(a,b) (((a)<(b))?(a):(b))
 #endif
 
 #ifndef MAX
-#define MAX(a,b) ((a>b)?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 #endif
 
 #define BMARK(fn) bmark_enqueue((#fn), (fn))
